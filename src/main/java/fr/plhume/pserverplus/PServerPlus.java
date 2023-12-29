5f39016a67c772da0cd2c7a075555e84735c4fec
Valentin B <72684309+Plhume@users.noreply.github.com>
2023-12-29 09:52:41 +0100

    Create singleton in main class

--- src/main/java/fr/plhume/pserverplus/PServerPlus.java
+++ src/main/java/fr/plhume/pserverplus/PServerPlus.java
@@ -4,10 +4,13 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PServerPlus extends JavaPlugin {
 
+    private static PServerPlus instance;
+
     @Override
     public void onEnable() {
         getLogger().info("PServerPlus by Plhume");
 
+        instance = this;
 
         getLogger().info("Plugin enabled!");
     }
@@ -15,6 +18,10 @@ public final class PServerPlus extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("Plugin disabled!");
+    }
+
+    public static PServerPlus getInstance() {
+        return instance;
     }
     }
 }
